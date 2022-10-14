4eea783d6c57901a3963e783e8923d762166c6bc
emanoelcampos <emanoel.cv@gmail.com>
2022-10-14 00:26:34 -0300

    criando cursos.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
+import { CursosModule } from './cursos/cursos.module';
 import { CursosService } from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CursosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CriarCursoModule
+    CriarCursoModule,
+    CursosModule
   ],
   providers: [CursosService],
   bootstrap: [AppComponent]
