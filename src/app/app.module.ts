c15e7a5f0b77b29aab68820c6d02248fb10584b4
emanoelcampos <emanoel.cv@gmail.com>
2022-10-14 16:44:24 -0300

    criando componente receber cursos criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
 import { CursosService } from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { ReceberCursoCriadoComponent } from './receber-curso-criado/receber-curso-criado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ReceberCursoCriadoComponent,
   ],
   imports: [
     BrowserModule,
